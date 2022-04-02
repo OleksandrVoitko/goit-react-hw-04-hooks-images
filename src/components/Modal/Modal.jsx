2aca5a8718bf7dff3ecbc5bcced5bf5552b05b23
OleksandrVoitko <voytkomail@gmail.com>
2022-04-02 15:30:48 +0300

    refactoring of component Modal is done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,29 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import { Overlay, ModalImg } from "./Modal.styled";
 
 const modalRoot = document.querySelector("#modal-root");
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
+export default function Modal({ onClose, largeImg }) {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (event) => {
-    if (event.code === "Escape") {
-      console.log("Escape");
-      this.props.onClose();
-    }
+  const handleKeyDown = (event) => {
+    if (event.code === "Escape") onClose();
   };
 
-  handleClickBackdrop = (event) => {
-    if (event.currentTarget === event.target) {
-      this.props.onClose();
-    }
+  const handleClickBackdrop = (event) => {
+    if (event.currentTarget === event.target) onClose();
   };
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleClickBackdrop}>
-        <ModalImg>
-          <img src={this.props.largeImg} alt="" />
-        </ModalImg>
-      </Overlay>,
-      modalRoot
-    );
-  }
+
+  return createPortal(
+    <Overlay onClick={handleClickBackdrop}>
+      <ModalImg>
+        <img src={largeImg} alt="" />
+      </ModalImg>
+    </Overlay>,
+    modalRoot
+  );
 }
-
-export default Modal;
